bd18ecb2f19164288e598cad79c7164972735ff7
Yevhen Yaremchuk <yyaremchuk@gmail.com>
2015-04-14 17:42:56 +0100

    Initial commit

---
+++ src/main/java/com/somecompany/detector/App.java
@@ -0,0 +1,13 @@
+package com.somecompany.detector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
