22b64422995c055a0331a86dbe70774ba9b2f904
Yevhen Yaremchuk <yyaremchuk@gmail.com>
2015-04-15 23:09:20 +0100

    First implementation

---
+++ src/main/java/com/somecompany/detector/HackerDetector.java
@@ -0,0 +1,86 @@
+/**
+ * com.somecompany.detector.HackerDetector.java
+ * Apr 15, 2015
+ * anti-hacker
+ *
+ */
+package com.somecompany.detector;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yyaremchuk
+ *
+ */
+public class HackerDetector {
+	private Map<String, Entry> attempts;
+
+	public HackerDetector() {
+		this.attempts = new HashMap<String, Entry>();
+	}
+	
+	/**
+	 * Parse log line of the following format: ip,date,action,username
+	 * @param line
+	 * @return
+	 */
+	public String parseLine(String line) {
+		final String[] tokens = line.split(",");
+
+		if (tokens.length == 4) {
+			final long timestamp = Long.valueOf(tokens[1]);
+
+			if ("SIGNIN_FAILURE".equals(tokens[2])) {
+				final Entry entry = attempts.get(tokens[0]);
+
+				if (entry == null || entry.isExpired(timestamp)) {
+					attempts.put(tokens[0], new Entry(timestamp));
+				} else {
+					entry.registerFailure();
+
+					if (!entry.isExpired(timestamp) && entry.getFailures() == 5) {
+						return tokens[0];
+					}
+ 				}
+			}
+		}
+
+		return null;
+	}
+
+	public int getFailures(String ip) {
+		final Entry entry = attempts.get(ip); 
+		return entry != null ? entry.getFailures() : 0;
+	}
+
+	public static class Entry {
+		private long timestamp;
+		private int failures;
+		
+		public Entry(long timestamp) {
+			this.timestamp = timestamp;
+			this.failures = 1;
+		}
+
+		public void resetFailures() {
+			this.failures = 1;
+		}
+
+		public long getTimestamp() {
+			return timestamp;
+		}
+
+		public int getFailures() {
+			return failures;
+		}
+
+		public boolean isExpired(long now) {
+			return now - timestamp > 300000L;
+		}
+
+		public void registerFailure() {
+			this.failures++;
+		}
+	}
+}
---
+++ src/test/java/com/somecompany/detector/HackerDetectorTest.java
@@ -0,0 +1,88 @@
+/**
+ * com.somecompany.detector.HackerDetectorTest.java
+ * Apr 14, 2015
+ * anti-hacker
+ *
+ */
+package com.somecompany.detector;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.*;
+
+/**
+ * @author yyaremchuk
+ *
+ */
+public class HackerDetectorTest {
+	
+	@Test
+	public void testNothing() {
+		System.out.println("OK");
+	}
+
+	@Test
+	public void testParseLine() {
+		final HackerDetector detector = new HackerDetector();
+		assertNull(detector.parseLine("80.238.9.179,133612947,SIGNIN_SUCCESS,Dave.Branning"));
+	}
+	
+	@Test
+	public void testCatchingAHacker() {
+		final HackerDetector detector = new HackerDetector();
+
+		final Calendar calendar = Calendar.getInstance();
+		assertNull(detector.parseLine("80.238.9.179," + calendar.getTimeInMillis() + ",SIGNIN_FAILURE,Dave.Branning"));
+		
+		calendar.add(Calendar.SECOND, 40);
+		assertNull(detector.parseLine("80.238.9.179," + calendar.getTimeInMillis() + ",SIGNIN_FAILURE,Dave.Branning"));
+		
+		calendar.add(Calendar.SECOND, 40);
+		assertNull(detector.parseLine("80.238.9.179," + calendar.getTimeInMillis() + ",SIGNIN_FAILURE,Dave.Branning"));
+		
+		calendar.add(Calendar.SECOND, 40);
+		assertNull(detector.parseLine("80.238.9.179," + calendar.getTimeInMillis() + ",SIGNIN_FAILURE,Dave.Branning"));
+		
+		calendar.add(Calendar.SECOND, 40);		
+		assertEquals("80.238.9.179", detector.parseLine("80.238.9.179," + calendar.getTimeInMillis() + ",SIGNIN_FAILURE,Dave.Branning"));
+	}
+
+	@Test
+	public void testGetFailedAttempts() {
+		final HackerDetector detector = new HackerDetector();
+		assertEquals(0, detector.getFailures("80.238.9.179"));
+
+		detector.parseLine("80.238.9.179,133613010,SIGNIN_FAILURE,Dave.Branning");
+		assertEquals(1, detector.getFailures("80.238.9.179"));
+
+		detector.parseLine("80.238.9.179,133613010,SIGNIN_SUCCESS,Dave.Branning");
+		assertEquals(1, detector.getFailures("80.238.9.179"));
+	}
+
+	@Test
+	public void testFiveFailuresWithinSixMinutes() {
+		final Calendar calendar = Calendar.getInstance();
+		final HackerDetector detector = new HackerDetector();
+
+		assertNull(detector.parseLine("80.238.9.179," + calendar.getTimeInMillis() + ",SIGNIN_FAILURE,Dave.Branning"));
+		assertEquals(1, detector.getFailures("80.238.9.179"));
+		
+		calendar.add(Calendar.MINUTE, 1);		
+		assertNull(detector.parseLine("80.238.9.179," + calendar.getTimeInMillis() + ",SIGNIN_FAILURE,Dave.Branning"));
+		assertEquals(2, detector.getFailures("80.238.9.179"));
+		
+		calendar.add(Calendar.MINUTE, 1);
+		assertNull(detector.parseLine("80.238.9.179," + calendar.getTimeInMillis() + ",SIGNIN_FAILURE,Dave.Branning"));
+		assertEquals(3, detector.getFailures("80.238.9.179"));
+
+		calendar.add(Calendar.MINUTE, 2);
+		assertNull(detector.parseLine("80.238.9.179," + calendar.getTimeInMillis() + ",SIGNIN_FAILURE,Dave.Branning"));
+		assertEquals(4, detector.getFailures("80.238.9.179"));
+
+		calendar.add(Calendar.MINUTE, 2);
+		assertNull(detector.parseLine("80.238.9.179," + calendar.getTimeInMillis() + ",SIGNIN_FAILURE,Dave.Branning"));
+		assertEquals(1, detector.getFailures("80.238.9.179"));
+	}	
+}
